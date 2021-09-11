c8c2ac4661fcda4f213d4822deac79b3d42561d8
Sergio <sergioariasleon@gmail.com>
2021-09-11 15:17:25 -0500

    Routing set up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,36 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PagesRoutingModule } from './pages/pages.routing';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
 import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
-import { PagesComponent } from './pages/pages.component';
-import { ProgressComponent } from './pages/progress/progress.component';
+import { AuthRoutingModule } from './auth/auth.routing';
 
 
-const routes:Routes = [
-  {
-    path: '',
-    component: PagesComponent,
-    children: [
-      {path: 'dashboard', component: DashboardComponent},
-      {path: 'progress', component: ProgressComponent},
-      {path: 'grafica-1', component: Grafica1Component},
-      {path: '', redirectTo: '/dashboard', pathMatch: 'full'}
-    ],
-  },
-  
-  {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent},
-  
+const routes:Routes = [  
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   {path: '**', component: NopagefoundComponent}
 ];
 
 @NgModule({
   declarations: [],
   imports: [
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    PagesRoutingModule,
+    AuthRoutingModule
   ],
   exports: [RouterModule]
 })
--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-pages',
@@ -6,11 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class PagesComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class PagesComponent {
+  year = new Date().getFullYear();
 }
